9037853b2cf1a5ffaaa7010046e06fce4af3071a
Jason22web <ST10507997@myemeris.edu.za>
2026-04-24 22:25:37 +0200

    Third commit after completing some of the layout

--- app/src/main/java/com/example/imad5112assignment2/MainActivity.kt
+++ app/src/main/java/com/example/imad5112assignment2/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.imad5112assignment2
 
-import android.R
+
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,15 +18,42 @@ import com.example.imad5112assignment2.ui.theme.IMAD5112ASSIGNMENT2Theme
 class MainActivity : ComponentActivity() {
 
     private val questions = arrayOf(
-        ""
+        "Putting a small glass of water in the microwave with your pizza prevents the crust from getting chewy.",
+        "Entering your ATM PIN in reverse will silently alert the police and lock the machine during a robbery.",
+        "You can tell if an alkaline battery is full or empty by dropping it; empty batteries bounce higher.",
+        "Burying a wet smartphone in a bowl of dry white rice is the best way to extract internal moisture.",
+        "You can remove permanent marker from a whiteboard by drawing over it with a dry-erase marker.",
+        "Using Premium 91-octane gas in a car designed for Regular 87-octane will increase horsepower.",
+        "Rubbing a walnut on a scratch in wooden furniture can hide the scratch.",
+        "Holding the Close Door button and your floor number simultaneously in an elevator acts as a priority code.",
+        "Turning your toaster on its side to make a grilled cheese sandwich is a safe and effective hack.",
+        "Placing a wooden spoon across the top of a boiling pot of water prevents it from boiling over."
     )
 
+    private val answers = arrayOf(true, false, true, false, true, false, true, false, false, true)
+    private var currentQuestions = 0
+    private var score = 0
+    private val feedbackList= mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView() {
+        showWelcomeScreen()
+    }
 
+    private fun showWelcomeScreen() {
+        setContentView(R.layout.activity_welcome)
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            currentQuestions = 0
+            score = 0
+            feedbackList.clear()
+            showQuizScreen()
+        }
+    }
+
+    private fun showQuizScreen() {
+
+        val questionText = findViewById<Text>(R.id.txtQuestions)
         }
     }
 }
